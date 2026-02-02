62decf3257f7cfe74d85b721fd1e10faa4e3112a
Shivam Jha <39839432+Shivamjha98@users.noreply.github.com>
2026-02-02 21:24:57 +0530

    Add linear search function with test case
    Implement linear search function and test case

--- searching_programs/linear_search.py
+++ searching_programs/linear_search.py
@@ -1,1 +1,8 @@
+def linear_search(arr, item):
+    for i in range(len(arr)):
+        if arr[i] == item:
+            return i
+    return None
 
+# returning None if not found (-1 can be interpreted as last index)
+print(linear_search([1,2,3,4,5], 99))
